f0666fb7df5e7603c73a760965963237f2b2e79f
marcpaga <marc.pages@live.com>
2020-11-09 12:50:25 +0100

    Fix errors and add ML part

--- black_box_svm.R
+++ black_box_svm.R
@@ -3,19 +3,26 @@
 
 require(e1071)
 
-build_model <- function(data) {
-
+build_model <- function(data, labels) {
   
-  #print confusion matrix  
-  #return model object
+  if (sum(is.na(data)) > 0) {
+    stop('There are missing values in the data')
+  }
   
+  model <- svm(x = t(data), y = as.factor(labels))
+  return(model)
 }
 
 predict_new_data <- function(new_data, model) {
   
-  # error with missing values
+  predictions <- predict(model, t(new_data))
+  return(predictions)
+}
+
+performance <- function(labels, predictions) {
   
-  #print confusion matrix
+  print(table(predictions, labels))
+  
 }
 
 
